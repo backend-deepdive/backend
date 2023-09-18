3f08ef0c1ae7348026be8279cb7cb77ed01ffbf8
MinChangJeong <wjdalsckd777@naver.com>
2023-09-18 16:39:03 +0900

    [feat] modify root folder

--- src/main/java/com/stockcoin/StockcoinApplication.java
+++ src/main/java/com/stockcoin/StockcoinApplication.java
@@ -1,18 +1,11 @@
 package com.stockcoin;
 
-import com.api.global.config.ApplicationApiConfig;
-import com.api.socket.upbit.UpbitWsClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
-@Import({ApplicationApiConfig.class})
+@SpringBootApplication(scanBasePackages = {"com.api"})
 public class StockcoinApplication {
     public static void main(String[] args) {
         SpringApplication.run(StockcoinApplication.class, args);
-
-        UpbitWsClient client = new UpbitWsClient();
-        client.connect();
     }
 }
