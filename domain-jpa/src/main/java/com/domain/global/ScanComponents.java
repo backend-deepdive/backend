71027aa7a9e47bad5361a7737276b77505b247b2
MinChangJeong <wjdalsckd777@naver.com>
2023-10-09 18:36:54 +0900

    [feat] modify application properties

--- domain-jpa/src/main/java/com/domain/global/ScanComponents.java
+++ domain-jpa/src/main/java/com/domain/global/ScanComponents.java
@@ -4,8 +4,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@Configuration
 @EntityScan("com.domain.entity")
 @EnableJpaRepositories("com.domain.repository")
-@Configuration
 public class ScanComponents {
 }
